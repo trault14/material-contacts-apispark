513b311af05d376676748b730d60d2f13d58fa1b
Tim Rault <tim.rault@etudiant.mines-nantes.fr>
2016-10-26 14:53:30 +0200

    Add contact persistance test
    Created new class ContactPersistanceTest and added test #2

---
+++ src/test/java/net/apispark/webapi/db/ContactPersistenceTest.java
@@ -0,0 +1,33 @@
+package net.apispark.webapi.db;
+
+import net.apispark.webapi.representation.Contact;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by timrault on 2016-10-26.
+ */
+public class ContactPersistenceTest {
+
+    @Test
+    public void added_contact_is_correct() throws Exception {
+        //Given
+        Contact contact = new Contact(null, "Tim", "Rault", "svg-1", "Male");
+        //When
+        Contact result = ContactPersistence.INSTANCE.addContact(contact);
+        //Then
+        Assert.assertNotNull(contact.getId());
+    }
+
+    @Test
+    public void added_contact_is_well_retrieved_by_id() throws Exception {
+        //Given
+        Contact contact = new Contact(null, "Tim", "Rault", "svg-1", "Male");
+        Contact added = ContactPersistence.INSTANCE.addContact(contact);
+        //When
+        Contact retrieved = ContactPersistence.INSTANCE.getContact(added.getId());
+        //Then
+        Assert.assertEquals(added, retrieved);
+
+    }
+}
